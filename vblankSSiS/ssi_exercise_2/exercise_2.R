9e064aca0c398c2cd446569f58fc810be425e45b
Vincent Bare'encastle <53657534+TheFabulousDeath@users.noreply.github.com>
2024-04-26 10:53:39 +0200

    26.04. SSI Übung

--- vblankSSiS/ssi_exercise_2/exercise_2.R
+++ vblankSSiS/ssi_exercise_2/exercise_2.R
@@ -49,4 +49,43 @@
   for(i in samples){
     hist(i, breaks = seq(from=-5, to=5, by=0.1))    
   }
+
+#Aufgabe 2.6
   
+  #  
+  
+
+  muenze = function(N, K)
+  {
+    resN <- vector(mode = "numeric",length = N)
+    for(i in 1:N){
+      resK <- sample(2, size = K, replace=TRUE)*2-3
+      resN[i] = sum(resK)
+    }
+    return(resN/sqrt(K))
+  }
+
+  hist(muenze(100000,1), breaks = 10)
+  hist(muenze(100000,2), breaks = 50)
+  hist(muenze(100000,12), breaks = 100)
+  hist(muenze(100000,1000), breaks = 150)
+  
+#Aufgabe 2.7
+  
+  erf = function(x) {
+    return(2*pnorm(sqrt(2)*x) -1)
+  }
+  
+  norm2unif = function (N){
+    y <- rnorm(1000000, mean = 0, sd = 1) # EIGENTLICH MÜNZWURF
+    return(0.5 * erf(y/sqrt(2))+0.5)
+  }
+  
+  jakeSpectrum2 = function(n){
+    f = function(x) {sinpi(x-0.5)}
+    x = norm2unif(n)
+    return = f(x)
+  }
+  
+  hist(norm2unif(10**7),breaks = 150)
+  hist(jakeSpectrum2(10**6), breaks = 200)
